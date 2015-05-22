a8854b4162f04b3e74563380ffc4548fdc72c610
Joern Klinger <ungewiss@gmail.com>
2015-05-22 17:12:57 -0500

    Runs until last lines of naive_bayes_7_redux.py, then crashes

---
+++ naive_bayes_7_redux.py
@@ -0,0 +1,155 @@
+# TODO:
+# - make class assignment soft
+
+#
+# add support for more than two classes
+# multiple class sizes
+
+# Import modules
+import numpy as np
+from numpy import log
+import pdb
+import cPickle as pickle
+import json
+from sklearn.linear_model import LogisticRegression as lr
+
+# Import modules for BOWs
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# Import modules for sparse matrix operations
+from scipy.sparse import hstack
+from scipy.sparse import csr_matrix
+
+def logsumexp(a):
+    a = np.asarray(a)
+    a_max = a.max()
+    return a_max + np.log((np.exp(a - a_max)).sum())
+
+# Load the data from pickle file, load user_ids from pickle file as well
+# data: feature_vector
+f = open('feature_vector.pickle', 'r')
+feature_vector = pickle.load(f)
+f.close()
+
+g = open('irony_matrix.pickle', 'r')
+irony_matrix = pickle.load(g)
+g.close()
+
+h = open('authors.pickle', 'r')
+authors = pickle.load(h)
+h.close()
+
+# Create bag of words
+bigram_vectorizer = TfidfVectorizer(ngram_range=(1, 2), token_pattern=r'\b\w+\b', min_df=1)
+
+# extract text from comments
+comments_for_bow = []
+for row in irony_matrix:
+    comments_for_bow.append(row[3])
+
+# bag of words
+bow = bigram_vectorizer.fit_transform(comments_for_bow)
+
+# User classes vector. Specify the different amounts of user_classes that you wanna build models with
+user_classes_list = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+# --- Set Parameters ---
+
+# set parameter for models, summary and iterations and SD
+# number of different models, number of classes to divide users into
+
+models = 1
+iterations = 5
+sd = 0.0001
+
+# Initialize beta for logistic regression
+beta = np.log(np.random.normal(loc=0.5, scale=sd))
+
+# summary data file; contains stats for each model and each iteration
+summary = np.zeros([models * iterations * len(user_classes_list), 5])
+all_conditional_probabilities = []
+all_probabilities_user_in_class = []
+all_class_probabilities = []
+
+# Counts is basically the line number each new entry in the summary file
+counts = 0
+
+# == For each user_class in user_class_list ==
+# for user_class_nr in user_classes_list:
+user_classes = user_classes_list[1]  # fix to [0] here, change later, uncomment
+
+# Set model to 1 here, later: for loop
+model = 1
+
+# == for each model ==
+
+# for model in xrange(models):
+
+# Initialize last_probability_data_given_parameters_log as negative infinity
+last_probability_data_given_parameters_log = -np.inf
+
+# probabilities that a randomly drawn user is in class 0 or 1 [p_c1, p_c2, ..., p_cn]
+class_probabilities_log = np.ones(user_classes) * np.log(float(1) / user_classes)
+
+# conditional probabilities for each feature by class
+conditional_probabilities_log = np.empty([feature_vector.shape[1], user_classes], dtype=float)
+
+# for one class we initialize them all as log(float(1)/user_classes)
+# for the other classes we initialize them as a random close to log(float(1)/user_classes)
+for row in xrange(len(conditional_probabilities_log)):
+    conditional_probabilities_log[row, 0] = log(float(1) / user_classes)
+    for column in xrange(1, user_classes):
+        conditional_probabilities_log[row, column] = log(np.random.normal(loc=float(1) / user_classes, scale=sd))
+
+# Define for easier access
+nr_of_users = feature_vector.shape[0]
+nr_of_features = feature_vector.shape[1]
+
+# probability_user_in_class_log rows: users, cols: log([p(class0), p(class1), ..., p(classn)])
+# initialized to a random number
+probability_user_in_class_log = np.empty([nr_of_users, user_classes], dtype=float)
+
+# Randomly assigning users to groups
+for row in xrange(len(probability_user_in_class_log)):
+    for col in xrange(probability_user_in_class_log.shape[1]):
+        probability_user_in_class_log[row, col] = np.log(np.random.normal(loc=float(1) / user_classes, scale=sd))
+
+### Initialize the logreg
+
+# Y ~ intercept + user_class + bow + bow*userclass
+# Y ~ intercept + P(user_class 0) + P(user_class 1) + ... + P(user_class n) + bow + bow * P(user_class 0) + bow * P(user_class 1) + ... + P(user_class n)
+
+interaction = np.empty([bow.shape[0], bow.shape[1]*(user_classes)])
+intercept = np.ones(len(irony_matrix))
+userclass = np.empty([len(irony_matrix), user_classes])
+
+logreg_Y_init = np.empty([len(irony_matrix)])
+
+# for row in irony_matrix:
+#     user_ind = wo der user name in authors ist ist, das ist dann die  row im feature_vector
+for row in xrange(len(irony_matrix)):
+    user_ind = authors.index(irony_matrix[row][5])
+    userclass[row] = probability_user_in_class_log[user_ind]
+    logreg_Y_init[row] = irony_matrix[row][7]
+
+bow_dense = bow.todense()
+
+# We will use soft assignments for class: P(user in class1)
+
+# d is just a delimeter for the columns
+d = interaction.shape[1]/user_classes
+
+for row in xrange(bow_dense.shape[0]):
+    for c in xrange(user_classes):
+        interaction[row, (d*c) : (d*(c+1))] = bow_dense[row] * userclass[row, c]
+
+###
+X1 = np.empty([len(irony_matrix), 1+user_classes])
+X1[:,0] = intercept
+X1[:,1:user_classes+1] = userclass
+
+logreg_X_init = hstack((X1, bow, interaction))
+
+logreg = lr(class_weight='auto')  # Set lr.coef_ = []
+logreg.fit(logreg_X_init, logreg_Y_init)
+coef_init = logreg.coef_
+print('coef_init: ' + str(coef_init))
---
+++ prepara_data_redux.py
@@ -0,0 +1,147 @@
+# Some comments aren't labeled, so I need to use the labeled_comments_ids subset of comments
+# Use that to find the segments corresponding to those comments
+# Then do what I'm doing
+
+from annotation_stats import *
+import cPickle as cpickle
+from pandas import *
+from collections import Counter
+import pdb
+from string import lstrip
+
+labeled_comment_ids = get_labeled_thrice_comments()
+sentence_ids, subreddits = get_sentence_ids_for_comments(labeled_comment_ids)
+
+collapse_f = lambda lbl_set: 1 if lbl_set.count(1) >= 2 else -1
+sentence_ids, sentence_texts, sentence_lbls = get_texts_and_labels_for_sentences(
+        sentence_ids, repeat=False, collapse=collapse_f, add_punctuation_features_to_text=False)
+
+
+# get all authors
+authors = cursor.execute(
+            '''select redditor from irony_comment;''').fetchall()
+
+authors2 = []
+for a in authors:
+    authors2.append(a[0])
+authors = authors2
+
+# get past user comments
+
+irony_pastusercomment_tmp = cursor.execute(
+            '''select * from irony_pastusercomment;''').fetchall()
+irony_pastusercomment = DataFrame(irony_pastusercomment_tmp)
+irony_pastusercomment.columns = ['id', 'redditor', 'comment_text', 'subreddit', 'thread_title', 'thread_id', 'thread_url', 'upvotes', 'downvotes', 'date', 'permalink']
+
+# BUILD FEATURE VECTOR
+# !!! right now we are using all features, instead of only 500
+# TODO: select the 500 most popular ones
+
+subreddits = irony_pastusercomment['subreddit'].tolist()
+subreddits_500 = Counter(subreddits).most_common()[:500]
+subreddits = []
+for s in subreddits_500:
+    subreddits.append(s[0])
+
+# user_ids
+# all users to build feature vector
+
+# feature vector array,
+# rows = nr of users
+# columns = nr of total subreddits
+
+feature_vector_shape = (len(authors), len(subreddits))
+feature_vector = np.zeros(feature_vector_shape)
+
+# for each user, find the subreddits of that user
+# find the indices of those subreddits
+# make those columns in the feature vector 1
+
+for a in authors:
+    subreddits_of_a=list(set(irony_pastusercomment[irony_pastusercomment['redditor'] == a]['subreddit'].tolist()))
+    indices_of_subreddits = [i for i, x in enumerate(subreddits) if x in subreddits_of_a]
+    # print indices_of_subreddits
+    row = authors.index(a)
+    feature_vector[row,indices_of_subreddits] = 1
+
+
+# use
+# feature_vector[row,indices_of_subreddits] += 1
+# then count each columns
+# take top 500 columns
+
+
+
+# Dump feature fector into file
+
+f = open('feature_vector.pickle', 'w')
+cpickle.dump(feature_vector, f)
+f.close()
+
+# Dump authors as well
+
+h = open('authors.pickle', 'w')
+cpickle.dump(authors, h)
+h.close()
+
+# BUILD IRONY DATA FRAME
+# sentence_id, author_id, sentence_text, label
+
+# id, reddit_id, subreddit, thread_title, thread_urlm thread_id, redditor, parent_comment_id, to_label, downvotes, upvotes, permalink
+irony_comment = cursor.execute(
+            '''select * from irony_comment;''').fetchall()
+irony_comment2 = [list(u) for u in irony_comment]
+irony_comment = DataFrame(irony_comment2)
+irony_comment.columns = ['id', 'reddit_id', 'subreddit', 'thread_title', 'thread_url', 'thread_id', 'redditor', 'parent_comment_id', 'to_label', 'downvotes', 'upvotes', 'permalink']
+
+# id, segment_id, comment_id, labeler_id, label, forced_decision, viewed_thread, viewed_page, confidence
+irony_label = cursor.execute(
+            '''select * from irony_label;''').fetchall()
+irony_label2 = [list(u) for u in irony_label]
+irony_label = DataFrame(irony_label2)
+irony_label.columns = ['id', 'segment_id', 'comment_id', 'labeler_id', 'label', 'forced_decision', 'date', 'viewed_thread', 'viewed_page', 'confidence']
+
+
+# id, comment_id, segment_id, text
+irony_segment = cursor.execute(
+            '''select * from irony_commentsegment;''').fetchall()
+irony_segment2 = [list(u) for u in irony_segment]
+irony_segment = DataFrame(irony_segment2)
+irony_segment.columns = ['id', 'comment_id', 'segment_id', 'text']
+
+# WEIRD, sometimes list index out of range
+# id, comment_id, text, *user_id, subreddit, irony*
+irony_matrix = []
+
+# irony_segment['id'] (not ['segment_id'] !!!) corresponds to irony_label['segment_id']
+
+for s in irony_segment['id']:
+    # print s
+    c_id = int(irony_segment[irony_segment['id'] == s]['comment_id'])
+    s_id = int(irony_segment[irony_segment['id'] == s]['segment_id'])
+
+    # if comment is in labeled_comments then do below
+    if c_id in labeled_comment_ids:
+        ind = int(str(irony_segment[irony_segment['id'] == s]['text'])[0:5])
+        s_text = irony_segment[irony_segment['id'] == s]['text'][ind]
+
+        # get majority label
+        s_label_tmp = list(irony_label[(irony_label['comment_id'] == c_id) & (irony_label['segment_id'] == s)]['label'])
+        s_label = Counter(s_label_tmp).most_common()[0][0]
+
+        # get subreddit
+        ind = int(str(irony_comment[irony_comment['id'] == c_id]['subreddit'])[0:5])
+        c_subreddit = irony_comment[irony_comment['id'] == c_id]['subreddit'][ind]
+
+        # get author
+        ind = int(str(irony_comment[irony_comment['id'] == c_id]['redditor'])[0:5])
+        c_author = irony_comment[irony_comment['id'] == c_id]['redditor'][ind]
+        c_author_index = authors.index(c_author)
+
+        # add segment id
+        append_to_irony_matrix = [s, c_id, s_id, s_text, c_subreddit, c_author, c_author_index, s_label]
+        irony_matrix.append(append_to_irony_matrix)
+
+g = open('irony_matrix.pickle', 'w')
+cpickle.dump(irony_matrix, g)
+g.close()
